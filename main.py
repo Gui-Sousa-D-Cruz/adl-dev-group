059ac864da60ebd0d1272a5eef05c8709df45f3f
Julio Ramos <julio@jcsr.com.br>
2023-04-09 15:01:27 -0300

    added class sistema

---
+++ main.py
@@ -0,0 +1,5 @@
+from model.sistema import Sistema
+
+sist = Sistema('55646421688')
+existe = sist.get_sistema()
+sist.delete_sistema()
--- model/sistema.py
+++ model/sistema.py
@@ -1,39 +1,61 @@
 from openpyxl import Workbook, load_workbook
-filename = 'Sistemas.xlsx'
-wb = load_workbook(filename = filename)
+filename = './model/Sistemas.xlsx'
+wb = load_workbook(filename=filename)
 ws = wb.active
 
 class Sistema:
 
-    def __init__(self, codigo, servico=None):
-        self.__codigo = codigo
-        if servico is None:
-            self.__servico = ""
+
+
+    def __init__(self, codigo, nome=None):
+        self.codigo = codigo
+        if nome is None:
+            self.nome= ""
         else:
-            self.__servico = servico
+            self.nome = nome
 
-    def set_servico(self):
-        codigo = self.__codigo
-        servico = self.__servico
-        proxima_linha = ws.max_row + 1
-        ws.cell(row=proxima_linha, column=1).value = codigo
-        ws.cell(row=proxima_linha, column=2).value = servico
-        wb.save(filename = filename)
+    def set_sistema(self):
+        if self.get_sistema():
+            print('Sistema ja cadastrado!')
+        else:
+            codigo = self.codigo
+            nome = self.nome
+            proxima_linha = ws.max_row + 1
+            ws.cell(row=proxima_linha, column=1).value = codigo
+            ws.cell(row=proxima_linha, column=2).value = nome
+            wb.save(filename = filename)
 
-    def get_servico(self, codigo):
+    def get_sistema(self):
         for linha in range(2, ws.max_row + 1):
-            if ws.cell(row=linha, column=1).value == codigo:
-                nome_servico = ws.cell(row=linha, column=2).value
-                return (codigo, nome_servico)
+            if ws.cell(row=linha, column=1).value == self.codigo:
+                nome_sistema = ws.cell(row=linha, column=2).value
+                return f'Sistema encontrado! \n Código => {self.codigo} \n Nome: {nome_sistema}'
         print('Servico não encontrado')
         return None
 
-# # Instancia um objeto da classe Sistema
-sistema = Sistema('52452652', 'Servico Teste 1')
-#
-# # Chama o método set_servico() para salvar o objeto na planilha do Excel
-sistema.set_servico()
+    def update_sistema(self, novo_nome):
+        if self.get_sistema():
+            for linha in range(2, ws.max_row + 1):
+                if ws.cell(row=linha, column=1).value == self.codigo:
+                    ws.cell(row=linha, column=2).value = novo_nome
+                    wb.save(filename=filename)
+            print('Sistema Atualizado!')
+        else:
+            print('Sistema não encontrado, impossivel atualizar')
 
+    def delete_sistema(self):
+        if self.get_sistema():
+            for linha in range(2, ws.max_row + 1):
+                if ws.cell(row=linha, column=1).value == self.codigo:
+                    # ws.cell(row=linha, column=3).value = 1
+                    ws.delete_rows(linha)
+                    wb.save(filename=filename)
+            print('Sistema Deletado!')
+        else:
+            print('Sistema não encontrado, impossivel deletar')
+
+    def __str__(self):
+        return f'Código => {self.codigo} Nome => {self.sistema}'
 
 
 
